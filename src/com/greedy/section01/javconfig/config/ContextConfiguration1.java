05add143277465457c382133242f0bbd7e2af4c0
tj <tj@DESKTOP-MOB8F55>
2021-04-30 17:48:50 +0900

    주석 추가

--- src/com/greedy/section01/javconfig/config/ContextConfiguration1.java
+++ src/com/greedy/section01/javconfig/config/ContextConfiguration1.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 /* 스프링 스캐닝으로 bean으로 등록할  컴포넌트 @Conponent를 달아주며 ,
  * @Configuration도 @Conponent룰 상속받음
- * basePackage에 등록되지 않으 패키지는 스캔에서 제화ㅣ하고 ㅡ 등록된 패키지 내의 @Conponent 및 @Conponent
- * 를 상속 받은 어노체이션들을 탐색한다.
+ * basePackage에 등록되지 않으 패키지는 스캔에서 제외하고 등록된 패키지 내의 @Conponent 및 @Conponent
+ * 를 상속 받은 어노테이션들을 탐색한다.
  * 이때 basePakage를  등록하지 않으면  현 설정 클래스가 존재하는 패키지지를 자동으로 basePackage로 설정 한다.
  * 문자열 배열 형태로 여러 basePackge를 등록 할 수  있다.
  * ex)basePackages={"full-name1" , "full-name2"}*/
